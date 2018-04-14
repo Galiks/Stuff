cf4eca6ee8e66bdf75c651bfe2d49b2b33da4ca7
galiks <Galiks@mail.ru>
2018-04-14 13:57:14 +0400

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    abstract class Transport
+    {
+
+    }
+
+    class Car:Transport
+    {
+
+    }
+
+    class Juguli:Car
+    {
+
+    }
+
+    class Lamborgini:Car
+    {
+
+    }
+
+    interface IConteinerCovar<out T>
+        where T: Transport
+    {
+
+    }
+
+    interface IConteinerContr<in T>
+        where T: Transport
+    {
+
+    }
+
+    interface ITest
+    {
+        void Move1(int t);
+    }
+
+    abstract class Test
+    {
+        public abstract int x { get; set; }
+
+        public abstract void Move2();
+
+      
+
+        public void Moveing(int t)
+        {
+            Console.WriteLine("Move");
+        }
+    }
+
+    class Hero : Test, ITest
+    {
+        public override int x
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public override void Move2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move1(int t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class Conteiner<t>:
+        IConteinerContr<t>, IConteinerCovar<t>
+        where t: Transport
+    {
+
+    }
+
+    class Device
+    {
+        public  void PressKey() { Console.WriteLine("Device"); }
+    }
+
+    class Keyboard:Device
+    {
+        public new void PressKey()
+        {
+            Console.WriteLine("Keyboard");
+        }
+
+        public int PressKey(char a) { Console.WriteLine($"Keyboard: {a}"); return 0; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IConteinerCovar<Transport> q1 = new Conteiner<Juguli>();
+
+            //IConteinerContr<Lamborgini> q2 = new Conteiner<Transport>();
+
+            Device k = new Keyboard();
+            k.PressKey();
+        }
+    }
+}
