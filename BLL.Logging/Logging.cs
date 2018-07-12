8db910c937ef9631aa6882e1381ca4f0aec318ae
Galiks <31892748+Galiks@users.noreply.github.com>
2018-07-12 06:41:40 +0400

    without UI

---
+++ BLL.Logging/Logging.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Logging
+{
+    public class Logging
+    {
+        public void WriteLoggingInFile(string text)
+        {
+            using (StreamWriter file = new StreamWriter("Logging.txt", true, Encoding.Default))
+            {
+                file.WriteLine($"{text} : {DateTime.Now}");
+
+                file.Close();
+            }
+        }
+    }
+}
